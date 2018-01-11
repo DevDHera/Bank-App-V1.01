948e6d6e3022e7e80f12c813912ce7ab7488d067
DevDHera <devdhera@gmail.com>
2018-01-12 01:11:27 +0530

    AngularFire v5 intergration

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseProvider } from '../../providers/firebase/firebase';
-import { FirebaseListObservable } from 'angularfire2/database';
+//import { FirebaseListObservable } from 'angularfire2/database-deprecated';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 
 /**
  * Generated class for the LoginPage page.
@@ -17,19 +20,25 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class LoginPage {
 
-  customerNICs: FirebaseListObservable<any[]>;
+  //customerNICs: FirebaseListObservable<any[]>;
+  customerNICs: Observable<any[]>;
   newNIC = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
-    this.customerNICs = this.firebaseProvider.getNIC();
+    this.customerNICs = this.firebaseProvider.getNIC().snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, value: c.payload.val() }));
+    });
+    //this.customerNICs = this.firebaseProvider.
   }
+
+  
 
   addNIC() {
     this.firebaseProvider.addNIC(this.newNIC);
   }
 
-  removeNIC(id) {
-    this.firebaseProvider.removeNIC(id);
+  removeNIC(key: string) {
+    this.firebaseProvider.removeNIC(key);
   }
 
   ionViewDidLoad() {
--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the FirebaseProvider provider.
@@ -11,20 +12,28 @@ import { AngularFireDatabase } from 'angularfire2/database';
 @Injectable()
 export class FirebaseProvider {
 
+  //itemsRef: AngularFireList<any>;
+
+
   constructor(public afd : AngularFireDatabase) {
    //console.log('Hello FirebaseProvider Provider');
+   //this.itemsRef = afd.list('/customerNIC/');
   }
 
   getNIC() {
+    //return this.afd.list('/customerNIC/').valueChanges();
     return this.afd.list('/customerNIC/');
+    //this.afd.list('customerNIC').snapshotChanges().map(changes => {
+      //return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    //});
   }
 
-  addNIC(nic) {
+  addNIC(nic: string) {
     this.afd.list('/customerNIC/').push(nic);
   }
 
-  removeNIC(id) {
-    this.afd.list('/customerNIC/').remove(id);
+  removeNIC(key: string) {
+    this.afd.list('/customerNIC/').remove(key);
   }
 
 }
